6af34c09a79892ad7752493d2a6a3e5f0089e5fe
Ivet Lopez <ivet19@live.com>
2025-04-03 21:50:24 +0200

    Create navigation menu in Header component and add styles

--- src/components/Header/getHeader.ts
+++ src/components/Header/getHeader.ts
@@ -2,7 +2,27 @@ const getHeader = (): HTMLElement => {
   const header = document.createElement("header");
   header.className = "main-header";
 
-  header.innerHTML = "<h1 class='main-title'>Monuments</h1>";
+  header.innerHTML = `
+    <h1 class='main-title'>Monuments</h1>
+    <nav>
+    <ul class="links">
+      <li>
+        <a class="link" href="/monuments">Monuments</a>
+      </li>
+      <li>
+        <a class="link" href="/add-new-monument">Add new monument</a>
+      </li>
+    </ul>
+  </nav>
+    `;
+
+  const links = header.querySelectorAll("a");
+
+  links.forEach((link) => {
+    if (window.location.pathname === link.getAttribute("href")) {
+      link.classList.add("link--current");
+    }
+  });
 
   return header;
 };
