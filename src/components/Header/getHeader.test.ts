8c0bb19705e3ce140668d0471abc478257df14d2
Ivet Lopez <ivet19@live.com>
2025-04-03 21:51:14 +0200

    Add links tests

--- src/components/Header/getHeader.test.ts
+++ src/components/Header/getHeader.test.ts
@@ -1,9 +1,13 @@
 import getHeader from "./getHeader";
 
+const screen = document.createElement("div");
+beforeEach(() => {
+  screen.innerHTML = "";
+});
+
 describe("Given the Header component", () => {
   describe("When it renders", () => {
     test("Then it should show 'Monuments' inside a heading", () => {
-      const screen = document.createElement("div");
       const expectedAppTitle = "Monuments";
 
       const Header = getHeader();
@@ -14,6 +18,44 @@ describe("Given the Header component", () => {
 
       expect(appTitle).not.toBeNull();
       expect(appTitle?.textContent).toBe(expectedAppTitle);
+    });
+
+    test("Then it should show 'Monuments' as a link", () => {
+      const expectedTextContent = "Monuments";
+      const expectedPath = "/monuments";
+
+      const Header = getHeader();
+
+      screen.appendChild(Header);
+
+      const appLinks = screen.querySelectorAll("a");
+      const links = [...appLinks];
+      const appLink = links
+        .filter((link) => link.textContent === "Monuments")
+        .at(0);
+
+      expect(appLink).not.toBeNull();
+      expect(appLink?.textContent).toBe(expectedTextContent);
+      expect(appLink?.pathname).toBe(expectedPath);
+    });
+
+    test("Then it should show 'Add new monument' as a link", () => {
+      const expectedTextContent = "Add new monument";
+      const expectedPath = "/add-new-monument";
+
+      const Header = getHeader();
+
+      screen.appendChild(Header);
+
+      const appLinks = screen.querySelectorAll("a");
+      const links = [...appLinks];
+      const appLink = links
+        .filter((link) => link.textContent === "Add new monument")
+        .at(0);
+
+      expect(appLink).not.toBeNull();
+      expect(appLink?.textContent).toBe(expectedTextContent);
+      expect(appLink?.pathname).toBe(expectedPath);
     });
   });
 });
