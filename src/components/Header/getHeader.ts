0e53fde27e15ff6c6788822f596f7626afca534b
Ivet Lopez <ivet19@live.com>
2025-04-04 20:34:29 +0200

    Delete mistaken ul list and add forgotten files from previous commit

--- src/components/Header/getHeader.ts
+++ src/components/Header/getHeader.ts
@@ -3,17 +3,19 @@ const getHeader = (): HTMLElement => {
   header.className = "main-header";
 
   header.innerHTML = `
-    <h1 class='main-title'>Monuments</h1>
-    <nav>
-    <ul class="links">
-      <li>
-        <a class="link" href="/monuments">Monuments</a>
-      </li>
-      <li>
-        <a class="link" href="/add-new-monument">Add new monument</a>
-      </li>
-    </ul>
-  </nav>
+    <div class="header-container">
+      <h1 class='main-title'>Monuments</h1>
+      <nav>
+        <ul class="links">
+          <li>
+            <a class="link" href="/monuments">Monuments</a>
+          </li>
+          <li>
+            <a class="link" href="/add-new-monument">Add new monument</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
     `;
 
   const links = header.querySelectorAll("a");
