996b673c3d9a8fc86776bc2a5f62540c90643ef1
Ivet Lopez <ivet19@live.com>
2025-04-05 14:16:11 +0200

    Create getMonuments fetch function

---
+++ src/client/getMonuments.ts
@@ -0,0 +1,15 @@
+import Monument from "./types";
+
+const getMonuments = async (): Promise<Monument[]> => {
+  const response = await fetch(
+    `https://monuments-back-1.onrender.com/monuments`,
+  );
+
+  const { monuments } = (await response.json()) as {
+    monuments: Monument[];
+  };
+
+  return monuments;
+};
+
+export default getMonuments;
